26ede429f7112cb4444b0eaa1529a98f1550e57f
Hironori Watanabe <h.w.watanabe@gmail.com>
2012-03-18 22:39:41 +0900

    グループの作成を追加,bootstrap修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,6 +5,17 @@ class GroupsController < ApplicationController
     @title = "グループの作成|Groupquiz:クイズで楽しく情報共有"
   end
 
+  def create
+    @group = Group.new(params[:group])
+    if @group.save
+      flash[:success] = "登録に成功しました"
+      redirect_to @group
+    else
+      @title = "グループの作成|Groupquiz:クイズで楽しく情報共有"
+      render 'new'
+    end
+  end
+
   def show
     @group = Group.find(params[:id])
     @title = @group.title
--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -10,7 +10,9 @@ describe GroupsController do
     end
     it "タイトルの検証" do
       get 'new'
-      response.should have_selector("title", :content => "グループの作成|Groupquiz:クイズで楽しく情報共有")
+      response.should have_selector(
+        "title", :content => "グループの作成|Groupquiz:クイズで楽しく情報共有"
+      )
     end
     it "タイトル入力欄があること" do
       get 'new'
@@ -21,6 +23,7 @@ describe GroupsController do
   describe "GET 'show'" do
     before(:each) do
       @group = Factory(:group)
+      @current_user = {:name => "Hironori Watanabe"}
     end
     it "returns http success" do
       get 'show', :id => @group
@@ -35,5 +38,45 @@ describe GroupsController do
       response.should have_selector("title", :content => @group.title)
     end
   end # get 'show'
+
+  describe "POST 'create'" do
+    describe "登録させないケースの検証" do 
+      before(:each) do 
+        @attr = {:user_id => 1, :title => "" } 
+      end 
+      it "グループを登録しないこと" do 
+        lambda do 
+          post :create, :group => @attr
+        end.should_not change(Group, :count)
+      end 
+      it "新規登録ページに戻ること(タイトル検証)" do 
+        post :create, :group => @attr
+        response.should have_selector("title", :content => "グループの作成|Groupquiz:クイズで楽しく情報共有") 
+      end 
+      it "新規登録ページに戻ること(テンプレート検証)" do 
+        post :create,  :group => @attr
+        response.should render_template('new') 
+      end 
+    end #登録させないケースの検証 
+
+    describe "登録に成功するケースの検証" do 
+      before(:each) do 
+        @attr = { :user_id => 1, :title => "グループ作成のテスト"} 
+      end 
+      it "DBに新規グループを登録していること" do 
+        lambda do 
+          post :create, :group => @attr
+        end.should change(Group, :count).by(1) 
+      end 
+      it "グループページに遷移していること" do 
+        post :create, :group => @attr
+        response.should redirect_to(group_path(assigns(:group))) 
+      end 
+      it "登録に成功した旨をユーザーに表示していること" do 
+        post :create, :group => @attr
+        flash[:success].should =~ /登録に成功しました/ 
+      end
+    end #登録に成功するケースの検証 
+  end
 
 end
