38bded22da53606899370f5c7b110aeffc642e1e
root <root@rubydev-virtual-machine.(none)>
2012-03-20 02:05:41 +0900

    edit機能、update機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,5 +21,15 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update_attributes(params[:user]) 
+        format.html { redirect_to @user, notice: 'Blog was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 end
