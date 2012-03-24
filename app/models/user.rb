a3588745b0cfac1f9e2ebeaa7ab2d48472168889
Hironori Watanabe <h.w.watanabe@gmail.com>
2012-03-24 13:46:01 +0900

    User modelの修正, user_controllerの文言修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :birthday, :sex, :email_address, :password
   has_many :belongs_to_groups, :foreign_key => "user_id", :dependent => :destroy
   has_many :groups, :through => :belongs_to_users
 
