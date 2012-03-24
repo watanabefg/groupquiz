ff0d77c69464157343ecae0cb57c0780fe3539fa
Hironori Watanabe <h.w.watanabe@gmail.com>
2012-03-24 13:55:42 +0900

    テーブル追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120321172002) do
+ActiveRecord::Schema.define(:version => 20120324044742) do
 
   create_table "belongs_to_groups", :force => true do |t|
     t.integer  "user_id"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(:version => 20120321172002) do
   create_table "groups", :force => true do |t|
     t.integer  "user_id"
     t.string   "title"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "sexes", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
