128718f9142b32f190c9010488d0880eda536c18
Hironori Watanabe <h.w.watanabe@gmail.com>
2012-04-11 20:01:07 +0900

    @nishijin ユーザー登録の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,21 @@ class User < ActiveRecord::Base
   belongs_to :sex
 
   def self.create_with_omniauth(auth)
+    print auth
     create!do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.name = auth["info"]["name"]
+      user.email_address = auth["info"]["email"]
+      #if auth["gender"] === "male" then
+      #  user.sex_id = 1
+      #else
+      #  user.sex_id = 2
+      #end
+      # user.registration_date = 
+      user.Account_information_flag = 0
+      user.possession_medals = 5
+      user.number_of_login = 1
     end 
   end 
 
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,11 +1,26 @@
+#encoding:UTF-8
 require 'spec_helper'
 
 describe SessionsController do
 
+  describe "GET 'new'" do
+    it "redirectの確認" do
+      get 'new'
+      response.should redirect_to('/auth/facebook')
+    end
+  end
+
   describe "GET 'create'" do
     it "returns http success" do
-      get 'create'
-      response.should be_success
+      post 'create', :provider => 'facebook', :uid => '1'
+      response.should redirect_to('/users/1')
+    end
+  end
+
+  describe "GET 'destroy'" do
+    it "redirectの確認" do
+      get 'destroy'
+      response.should redirect_to('/signin')
     end
   end
 
