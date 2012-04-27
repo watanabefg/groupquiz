9e5590e85ba1d5c87ebf17b2d64b3d8f669af553
Hironori Watanabe <h.w.watanabe@gmail.com>
2012-04-27 23:26:44 +0900

    answersコントローラの修正

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,29 @@
 class AnswersController < ApplicationController
   def new
   end
+
   def create
+    data = params[:quiz]
+    quiz_id = data["id"]
+    quizzes = Quiz.where(:id => quiz_id)
+    quizzes.each {|q| @quiz = q}
+
+    @answer = data["quiz_answer_radio"]
+
+    @clear = false
+    if @quiz.quiz_answer_radio.to_s === @answer.to_s then
+      @clear = true
+    end
+
+    if @clear
+      # TODO:正解の時
+      # メダルを増やす
+      # Answer.newする
+    else
+      # TODO:間違いの時
+      # エラーメッセージつきで
+      # 元の質問にリダイレクトする
+    end
+    redirect_to '/quizzes/'+quiz_id
   end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,12 +1,28 @@
+#encoding:UTF-8
 require 'spec_helper'
 
 describe AnswersController do
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
-    end
+  describe "POST 'create'" do
+    describe "登録させないケースの検証" do 
+      before(:each) do 
+        @attr = {:user_id => 1, :quiz_id => 1, :quiz_answer_radio => "" } 
+      end 
+      it "答えを登録しないこと" do 
+        lambda do 
+          post :create, :answer => @attr
+        end.should_not change(Answer, :count)
+      end 
+
+      it "新規登録ページに戻ること(タイトル検証)" do 
+        post :create, :answer => @attr
+        response.should have_selector("title", :content => "グループの作成|Groupquiz:クイズで楽しく情報共有") 
+      end 
+     # it "新規登録ページに戻ること(テンプレート検証)" do 
+     #   post :create,  :group => @attr
+     #   response.should render_template('new') 
+     # end 
+    end #登録させないケースの検証 
   end
 
 end
