98db53fef1ceb81003c035440e1562147c068be2
Hironori Watanabe <h.w.watanabe@gmail.com>
2012-05-22 14:08:44 -0700

    modelのattr_accessibleにgroup_category_radioを追加

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-  attr_accessible :user_id, :title
+  attr_accessible :user_id, :title, :group_category_radio
   validates :user_id, :presence => true
   validates :title, :presence => true, :uniqueness => true, :length => { :within => 6..50 }
   has_many :belongs_to_groups, :foreign_key => "group_id", :dependent => :destroy
